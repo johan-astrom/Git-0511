1b26a1b53c40a79af1efb20606134aba17f68dea
Johan Astrom <johan.astrom89@gmail.com>
2020-11-05 10:55:08 +0100

    New feature

--- src/com/johanastrom/Main.java
+++ src/com/johanastrom/Main.java
@@ -3,6 +3,6 @@ package com.johanastrom;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Second version");
+        System.out.println("Third version, new feature");
         }
 }
