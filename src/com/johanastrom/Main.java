df539d5c652cb85de496c397692567d7bae3652f
Johan Astrom <johan.astrom89@gmail.com>
2020-11-05 10:46:34 +0100

    Second commit

--- src/com/johanastrom/Main.java
+++ src/com/johanastrom/Main.java
@@ -3,6 +3,6 @@ package com.johanastrom;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
+        System.out.println("Second version");
+        }
 }
