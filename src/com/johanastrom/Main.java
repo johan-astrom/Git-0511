4488751081e778b0573512133e16b0138bf37421
Johan Astrom <johan.astrom89@gmail.com>
2020-11-09 20:51:18 +0100

    5th version, new feature

--- src/com/johanastrom/Main.java
+++ src/com/johanastrom/Main.java
@@ -3,6 +3,6 @@ package com.johanastrom;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Fourth version, new feature");
+        System.out.println("Fifth version, new feature");
         }
 }
