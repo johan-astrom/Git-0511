fa761090740b7f353f8bcb5e2eeaf5d5449344e2
Johan Astrom <johan.astrom89@gmail.com>
2020-12-07 21:37:54 +0100

    Fixed merge confl

--- src/com/johanastrom/Main.java
+++ src/com/johanastrom/Main.java
@@ -3,10 +3,7 @@ package com.johanastrom;
 public class Main {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Fifth version, Main branch");
-=======
         System.out.println("Sixth version, new feature");
->>>>>>> new_feature
+
         }
 }
